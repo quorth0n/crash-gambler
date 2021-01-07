b381b227a06f88dbe355bf7f5d3216414159ff14
quorth0n <lpoflynn@protonmail.ch>
2021-01-07 13:45:38 -0500

    10 btc bankroll

--- gameserver/server.js
+++ gameserver/server.js
@@ -13,7 +13,7 @@ var GameHistory = require("./server/game_history");
 
 var _ = require("lodash");
 
-require('dotenv').config();
+require("dotenv").config();
 
 var server;
 
@@ -56,7 +56,7 @@ async.parallel(
 
     var gameHistory = new GameHistory(results[0]);
     var info = results[1];
-    var bankroll = 1000*1e8;// results[2];
+    var bankroll = 10 * 1e8; // results[2];
 
     console.log("Have a bankroll of: ", bankroll / 1e8, " btc");
 
